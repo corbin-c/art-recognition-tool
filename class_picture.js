58fa5bf3edb120009ba7e88940dd5e875b6e03f3
Clément Corbin <corbin.clement@gmail.com>
2019-08-14 13:16:03 +0200

    1565781363

--- class_picture.js
+++ class_picture.js
@@ -4,7 +4,7 @@
 * or not) for the OpenCV Wrapper Worker, which handles image processing.
 * 
 */
-import OCV_Picture from "./worker_class_picture.js";
+import { OCV_Picture } from "./worker_class_picture.js";
 function Picture(imgData) {
   console.log("CS pic class instantiated");
   ocv = new OCV(imgData);
