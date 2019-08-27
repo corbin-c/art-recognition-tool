ae9312bedc214ba0229ccfd6112bf725a1c2f59b
Clément Corbin <corbin.clement@gmail.com>
2019-08-27 16:05:24 +0200

    rear camera only

--- class_camera.js
+++ class_camera.js
@@ -6,7 +6,7 @@ let Video = class {
   async getCamera(opencv_instance) {
     this.video.srcObject = await navigator.mediaDevices.getUserMedia({
         audio: false,
-        video: true
+        video: { facingMode: "environment" }
         });
     await (function(video) { return new Promise(
       function(resolve,reject) {
